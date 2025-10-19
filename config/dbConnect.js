8bd1c8691317fa83894304f78afa636f814ba3e8
sriram <sriramsriram6214@gmail.com>
2025-10-19 16:49:59 +0530

    Use Atlas URI via environment variable

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/quizDatabase', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
